784b65aaea0d600d15eefe6b19066aba3f60096f
Saoki11111 <t.aoki11111@gmail.com>
2020-09-01 09:28:38 +0900

    add

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,28 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all # テンプレート変数 acton method - view で値を渡す, Task.all -> tasks table から すべてのレコードを取得 → sql を書かずにすむ(裏で rails が sql を発行している)
+  end
+
+  def new
+    @task = Task.new # model task のインスタンスを生成する
+  end
+
+  def create
+    # ボタンを押したときにフォームの値をサーバに送信し、データをdb の保存させる
+    @task = Task.new(task_params) # form に入力されたデータを元にタスクモデルのインスタンスを作成する
+    if @task.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def task_params
+    # task_params params -> form から送られたデータ
+    # permit params 内の title カラムのみ db に書く 悪意のあるデータの書き換えを防ぐ
+    params[:task].permit(:title)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  resources :tasks # tasks の RESTFull なURL よく使う routing をまとめて用意してくれる設定
+  root 'tasks#index' # root URL でアクセス → tasks_controller の index method を実行
 end
